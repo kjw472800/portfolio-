60a2fb73e8a9f233150225028c2faa6d6d5f8022
JAFang <kjw472800@gmail.com>
2020-10-27 02:06:40 +0800

    INITIALIZE

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Switch, Route, Link as RouterLink, Redirect } from 'react-router-dom'
+import { Box,Button,Grid, Avatar,Link,Paper ,List,ListItem,ListItemIcon,ListItemText, Typography,SvgIcon, Divider, CssBaseline  } from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/Inbox';
+import Resume from './resume/Resume';
+
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+import HomeIcon from '@material-ui/icons/Home';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import WorkIcon from '@material-ui/icons/Work';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+function App() {
+
+
+
+  return (
+    <React.Fragment>
+        <CssBaseline/>
+
+
+  
+    </React.Fragment>
+
+  );
+}
+
+export default App;
---
+++ src/resume/Resume.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const Resume= ()=>{
+
+    return (
+        <div>
+            <Typography variant="body2" gutterBottom>
+                body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+                unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
+                dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+            </Typography>
+        </div>
+    )
+}
+
+export default Resume;
